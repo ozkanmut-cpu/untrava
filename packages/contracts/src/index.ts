b1de2a1f7df48b9f8e020ea1ac8ff49f2e9cbbc8
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-16 19:58:33 +0300

    feat: export alcohol goal types

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -3,6 +3,7 @@ export const CONTRACT_SCHEMA_VERSION = 1 as const;
 export * from './ids';
 export * from './core/goals';
 export * from './core/events';
+export * from './alcohol/goals';
 export * from './profile';
 export * from './events';
 export * from './consent';
