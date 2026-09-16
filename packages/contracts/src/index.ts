6cb58d70be6d68cc13ed3926a695979de5a3a918
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-16 17:09:57 +0300

    feat: export domain-neutral core contracts

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -1,6 +1,8 @@
 export const CONTRACT_SCHEMA_VERSION = 1 as const;
 
 export * from './ids';
+export * from './core/goals';
+export * from './core/events';
 export * from './profile';
 export * from './events';
 export * from './consent';
