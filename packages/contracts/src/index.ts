f446450582a29e4e78504b9a239a6a2d1d363de8
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-17 09:32:19 +0300

    refactor: expose core intervention infrastructure

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -3,6 +3,18 @@ export const CONTRACT_SCHEMA_VERSION = 1 as const;
 export * from './ids';
 export * from './core/goals';
 export * from './core/events';
+export {
+  InterventionLevelSchema,
+  InterventionBaseEligibilitySchema,
+  InterventionLibraryEnvelopeBaseSchema,
+  addInterventionLibraryIssues,
+} from './core/interventions';
+export type {
+  InterventionLevel,
+  InterventionBaseEligibility,
+  InterventionLibraryEnvelopeBase,
+  LibraryInterventionIdentity,
+} from './core/interventions';
 export * from './alcohol/goals';
 export * from './alcohol/events';
 export * from './alcohol/safety';
